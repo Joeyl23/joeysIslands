9f01080a54d8c093a20a83f18ff88c3e2e1e921f
Joey Larsen <joey2003larsen@gmail.com>
2022-06-24 18:42:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useMemo } from 'react';
 import './App.css';
 
-function App() {
+import * as THREE from 'three';
+import { Canvas, useLoader, useFrame} from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from '@react-three/drei';
+
+import worldgltf from './content/world.gltf'
+
+var mixer;
+
+
+function World(){
+  const gltf = useLoader(GLTFLoader, worldgltf);
+
+  mixer = new THREE.AnimationMixer(gltf.scene);
+
+  if (gltf.animations.length) {
+      mixer = new THREE.AnimationMixer(gltf.scene);
+      gltf.animations.forEach(clip => {
+          const action = mixer.clipAction(clip)
+          action.play();
+      });
+  }
+
+  useFrame((state, delta) => {
+      mixer?.update(delta)
+  })
+
+  return(
+    <primitive
+      object={gltf.scene}
+      position={[-4,1,1]}
+      />
+  );
+}
+
+const Stars = () => {
+  const count = 500;
+
+  const [positions, sizes] = useMemo(() => {
+    const positions = new Float32Array(count * 3);
+    const sizes = new Float32Array(count * 3);
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 10;
+      sizes[i] = Math.random() < 0.03 ? 15 : 6;
+    }
+
+    return [positions, sizes];
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <points>
+      <bufferGeometry>
+        <bufferAttribute
+          attachObject={["attributes", "position"]}
+          count={positions.length / 3}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <pointsMaterial size={0.1} />
+    </points>
+  );
+};
+
+function App() {
+
+  const [time,setTime] = useState('day');
+
+  function changeTime(){
+    if(time==='day'){
+      setTime('night');
+      document.body.style.backgroundColor = 'black'
+    }else{
+      setTime('day');
+      document.body.style.backgroundColor = '#dbe9fb'
+    }
+  }
+  return (
+    <>
+
+    <button className='timeButton' onClick={() =>changeTime()}> night / day</button>
+
+    <Canvas camera={{position:[-10,10,20]}}>
+      
+    <OrbitControls
+        enableZoom={true}
+        rotateSpeed={2}
+      />
+      <ambientLight intensity={0.4} />
+      <directionalLight
+      position={[0,10,0]}
+      />
+      <World />
+      <Stars />
+    </Canvas>
+
+
+    </>
   );
 }
 
